65a03255df07361a957313c4a67357db7e696021
Daniel Abello <abello.daniel@gmail.com>
2020-08-03 14:31:27 -0500

    Docs agregado

---
+++ docs/precache-manifest.f5c24bd669c3584b11be57e2f7e579bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3ef2278c5679dfd1c0f29e714a69f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f45e0411ae8a2aed31cd",
+    "url": "/static/css/main.f387f72d.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f45e0411ae8a2aed31cd",
+    "url": "/static/js/main.5188613a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
